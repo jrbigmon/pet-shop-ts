a806fb83fc488dfc434b5fa30edb735643a9660b
jrbigmon <juniorterriaga0@gmail.com>
2022-10-04 11:53:45 -0300

    Criado arquivo env e adicionado as configurações do sequelize

--- src/database/config.js
+++ src/database/config.js
@@ -1,7 +1,10 @@
+require('env').config()
+
 module.exports = {
-  "username": "root",
-  "password": null,
-  "database": "database_development",
-  "host": "127.0.0.1",
-  "dialect": "mysql"
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  port: process.env.DB_PORT
 }
